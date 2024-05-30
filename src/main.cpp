87e62a51ace3745cc9f6aae6a0f9e7e18c08e88f
Albrict <albrict.work@gmail.com>
2024-05-30 14:34:13 +0200

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,18 @@
 
 int main(int argc, char **argv)
 {
+
     if (argc != 2) 
         return 1;
     const bool result = VP::init();
     if (result == false)
         return 1;
-    VP::Window window(SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, "Title");
-    VP::Video video(argv[1], window);
-    video.play();
+    VP::Window window(SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, "Video player");
+    try {
+        VP::Video video(argv[1], window);
+        video.play();
+    } catch(std::exception &ex) {
+        std::cerr << ex.what() << '\n';
+    }
     VP::close();
 }
